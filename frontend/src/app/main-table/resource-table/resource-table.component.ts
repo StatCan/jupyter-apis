dd73ed3ccb237b3c433cd6cd86b8b25e4f1bed7a
saffaalvi <alvi118@uwindsor.ca>
2020-10-02 17:35:03 -0400

    fix: Add status to pvc table + refactor

--- frontend/src/app/main-table/resource-table/resource-table.component.ts
+++ frontend/src/app/main-table/resource-table/resource-table.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from "@angular/core";
 import { MatTableDataSource } from "@angular/material/table";
 import { Resource } from "src/app/utils/types";
+import {MatDialog} from "@angular/material/dialog";
+import {ConfirmDialogComponent} from "../confirm-dialog/confirm-dialog.component";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: "app-resource-table",
@@ -22,7 +25,10 @@ export class ResourceTableComponent implements OnInit {
     "actions"
   ];
   dataSource = new MatTableDataSource();
-  constructor() {}
+
+  constructor(
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {  }
 
@@ -32,8 +38,28 @@ export class ResourceTableComponent implements OnInit {
   connectResource(rsrc: Resource): void {
     window.open(`/notebook/${rsrc.namespace}/${rsrc.name}/`);
   }
-
-  deleteResource(rsrc:Resource){
-    this.deleteNotebookEvent.emit(rsrc);
+  
+  deleteResource(rsrc: Resource): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: "fit-content",
+      data: {
+        title: "You are about to delete Notebook Server: " + rsrc.name,
+        message:
+          "Are you sure you want to delete this Notebook Server? " +
+          "Your data might be lost if the Server is not backed by persistent storage.",
+        yes: "delete",
+        no: "cancel"
+      }
+    });
+    dialogRef
+      .afterClosed()
+      .pipe(first())
+      .subscribe(result => {
+        if (result !== "delete") {
+          return;
+        }
+        this.deleteNotebookEvent.emit(rsrc);
+      });
   }
+
 }
--- frontend/src/app/main-table/volumes-table/volume-table.component.ts
+++ frontend/src/app/main-table/volumes-table/volume-table.component.ts
@@ -1,21 +1,72 @@
-import {Component, Input, Output, EventEmitter} from "@angular/core";
+import {Component, Input, Output, EventEmitter, OnChanges, SimpleChanges} from "@angular/core";
 import {MatTableDataSource} from "@angular/material/table";
-import {Pvc} from "../../utils/types";
+import {Volume} from "../../utils/types";
+import {MatDialog} from "@angular/material/dialog";
+import {ConfirmDialogComponent} from "../confirm-dialog/confirm-dialog.component";
+import {first} from "rxjs/operators";
+
+export type PvcWithStatus = {
+  pvc: Volume;
+  mountedBy: string | null;
+}
 
 @Component({
   selector: "app-volume-table",
   templateUrl: "./volume-table.component.html",
   styleUrls: ["./volume-table.component.scss", "../main-table.component.scss"]
 })
-export class VolumeTableComponent {
-  @Input() custompvcs: Pvc[];
-  @Output() deletePvcEvent = new EventEmitter<Pvc>();
+export class VolumeTableComponent implements OnChanges {
+  @Input() pvcProperties: PvcWithStatus[];
+  @Output() deletePvcEvent = new EventEmitter<PvcWithStatus>();
 
   // Table data
-  displayedColumns: string[] = ["name", "size", "mountedBy", "actions"];
+  displayedColumns: string[] = ["status", "name", "size", "mountedBy", "actions"];
   dataSource = new MatTableDataSource();
 
-  deletePvc(pvc: Pvc) {
-    this.deletePvcEvent.emit(pvc);
+  deleteStatus: Set<string> = new Set<string>();
+
+  constructor(
+    private dialog: MatDialog
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.pvcProperties) {
+      const pvcNames = (changes.pvcProperties
+        .currentValue as PvcWithStatus[]).map(p => p.pvc.name);
+      this.deleteStatus.forEach(name => {
+        if (!pvcNames.includes(name)) {
+          this.deleteStatus.delete(name);
+        }
+      });
+    }
+  }
+
+  deletePvc(pvc: PvcWithStatus): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: "fit-content",
+      data: {
+        title: "You are about to delete the volume: " + pvc.pvc.name,
+        message:
+          "Are you sure you want to delete this volume? " +
+          "This action can't be undone.",
+        yes: "delete",
+        no: "cancel"
+      }
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(first())
+      .subscribe(result => {
+        if (result !== "delete") {
+          return;
+        }
+        this.deleteStatus.add(pvc.pvc.name);
+        this.deletePvcEvent.emit(pvc);
+      });
+  }
+
+  checkDeletionStatus(pvc: PvcWithStatus): boolean {
+    return this.deleteStatus.has(pvc.pvc.name);
   }
 }

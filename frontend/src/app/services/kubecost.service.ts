94be6f978722bead6823479d572b0ebb9cf9d5a0
saffaalvi <alvi118@uwindsor.ca>
2020-11-06 13:24:57 -0500

    fix: add check for kubecost error response + translation

--- frontend/src/app/services/kubecost.service.ts
+++ frontend/src/app/services/kubecost.service.ts
@@ -1,84 +1,73 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 
-import { Observable, throwError } from "rxjs";
-import { tap, catchError } from "rxjs/operators";
-import { environment } from "src/environments/environment";
-
-import {
-  Resp,
-  SnackType
-} from "../utils/types";
-import { SnackBarService } from "./snack-bar.service";
+import {Observable, throwError} from "rxjs";
+import {tap, catchError} from "rxjs/operators";
+import {environment} from "src/environments/environment";
+import {Resp} from "../utils/types";
 
 export type AggregateCostResponse = {
-  code: number,
+  code: number;
   data: {
     [namespace: string]: {
-      aggregation: string,
-      environment: string,
-      cpuAllocationAverage: number,
-      cpuCost: number,
-      cpuEfficiency: number,
-      efficiency: number,
-      gpuAllocationAverage: number,
-      gpuCost: number,
-      ramAllocationAverage: number,
-      ramCost: number,
-      ramEfficiency: number,
-      pvAllocationAverage: number,
-      pvCost: number,
-      networkCost: number,
-      sharedCost: number,
-      totalCost: number
-    }
-  },
-  message: string
-}
+      aggregation: string;
+      environment: string;
+      cpuAllocationAverage: number;
+      cpuCost: number;
+      cpuEfficiency: number;
+      efficiency: number;
+      gpuAllocationAverage: number;
+      gpuCost: number;
+      ramAllocationAverage: number;
+      ramCost: number;
+      ramEfficiency: number;
+      pvAllocationAverage: number;
+      pvCost: number;
+      networkCost: number;
+      sharedCost: number;
+      totalCost: number;
+    };
+  };
+  message: string;
+};
 
 @Injectable()
 export class KubecostService {
-
-  constructor(private http: HttpClient, private snackBar: SnackBarService) { }
+  constructor(private http: HttpClient) {}
 
   getAggregateCost(ns: string): Observable<AggregateCostResponse> {
-    const url = environment.apiUrl + `/api/namespaces/${ns}/cost/aggregated`
-    
-    return this.http.get<AggregateCostResponse>(url, {
-      params: {
-        aggregation: 'namespace',
-        namespace: ns,
-        window: '1d'
-      }
-    }).pipe(
-      tap(res => this.handleBackendError(res)),
-      catchError(err => this.handleError(err))
-    );
+    const url = environment.apiUrl + `/api/namespaces/${ns}/cost/aggregated`;
+
+    return this.http
+      .get<AggregateCostResponse | Resp>(url, {
+        params: {
+          aggregation: "namespace",
+          namespace: ns,
+          window: "1d"
+        }
+      })
+      .pipe(
+        tap(res => this.handleBackendError(res)),
+        catchError(err => this.handleError(err))
+      ) as Observable<AggregateCostResponse>;
   }
 
-
-  // ---------------------------Error Handling----------------------------------
-  private handleBackendError(response: { code: number }) {
-    if (response.code < 200 || response.code >= 300) {
+  private handleBackendError(response: AggregateCostResponse | Resp) {
+    if (this.isResp(response) || response.code < 200 || response.code >= 300) {
       throw response;
     }
   }
 
-  private handleError(error: HttpErrorResponse | Resp): Observable<never> {
-    // The backend returned an unsuccessful response code.
-    // The response body may contain clues as to what went wrong,
-    if (error instanceof HttpErrorResponse) {
-      this.snackBar.show(
-        `${error.status}: There was an error trying to connect ` +
-        `to the backend API. ${error.message}`,
-        SnackType.Error
-      );
-      return throwError(error.message);
-    } else {
-      // Backend error thrown from handleBackendError
-      const backendError = error as Resp;
-      this.snackBar.show(backendError.log, SnackType.Error);
-      return throwError(backendError.log);
-    }
+  private handleError(
+    error: HttpErrorResponse | AggregateCostResponse | Resp
+  ): Observable<never> {
+    const message = this.isResp(error) ? error.log : error.message;
+    return throwError(new Error(message));
+  }
+
+  private isResp(
+    obj: HttpErrorResponse | AggregateCostResponse | Resp
+  ): obj is Resp {
+    return (obj as Resp).success !== undefined;
   }
 }

8f0df98d3d37366ef34dd6c0eeabb5b929260138
Jose Matsuda <jose.matsuda@statcan.gc.ca>
2022-09-23 11:29:10 -0400

    Wendy changes
    https://github.com/StatCan/jupyter-apis/pull/51/files
    I think this is good, would want to confirm im a bit unsure

--- frontend/jupyter/src/app/pages/form/form-default/volume/volume.component.ts
+++ frontend/jupyter/src/app/pages/form/form-default/volume/volume.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
+import { FormGroup, Validators, ValidatorFn, AbstractControl, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Volume } from 'src/app/types';
-import { updateNonDirtyControl } from 'kubeflow';
+import { updateNonDirtyControl, NamespaceService } from 'kubeflow';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { JWABackendService } from "src/app/services/backend.service";
 
 @Component({
   selector: 'app-volume',
@@ -12,15 +14,20 @@ import { updateNonDirtyControl } from 'kubeflow';
 export class VolumeComponent implements OnInit, OnDestroy {
   private notebookNamePrv = '';
   private defaultStorageClassPrv: boolean;
+  private mountedVolumes: Set<string> = new Set<string>(); // AAW specific
 
   currentPVC: Volume;
   existingPVCs: Set<string> = new Set();
 
   subscriptions = new Subscription();
 
+  // Specific error matcher for volume name, AAW
+  matcher = new PvcErrorStateMatcher();
+
   // ----- @Input Parameters -----
   @Input() volume: FormGroup;
   @Input() namespace: string;
+  @Input() sizes: Set<string>; //AAW
 
   @Input()
   get notebookName() {
@@ -110,9 +117,21 @@ export class VolumeComponent implements OnInit, OnDestroy {
   }
 
   // ----- Component Functions -----
-  constructor() {}
+  constructor(
+    private backend: JWABackendService,
+    private ns: NamespaceService) {}
 
   ngOnInit() {
+    // AAW specific
+    this.volume
+      .get("name")
+      .setValidators([
+        Validators.required,
+        this.isMountedValidator(),
+        Validators.pattern(/^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$/)
+      ]);
+    // end AAW specific
+
     // type
     this.subscriptions.add(
       this.volume.get('type').valueChanges.subscribe((type: string) => {
@@ -128,6 +147,19 @@ export class VolumeComponent implements OnInit, OnDestroy {
         this.updateVolInputFields();
       }),
     );
+
+    //AAW specific: Get the list of mounted volumes of the existing Notebooks in the selected Namespace
+    this.subscriptions.add(
+      this.ns.getSelectedNamespace().subscribe(ns => {
+        this.backend.getNotebooks(ns).subscribe(notebooks => {
+          this.mountedVolumes.clear();
+          notebooks.map(nb => nb.volumes.map(v => {
+            this.mountedVolumes.add(v)
+          }));
+        });
+      })
+    );
+    // end AAW
   }
 
   ngOnDestroy() {
@@ -171,4 +203,34 @@ export class VolumeComponent implements OnInit, OnDestroy {
       this.volume.controls.name.setValue(this.currentVolName);
     }
   }
+  // AAW SPECIFIC CHANGES
+  showNameError() {
+    const volumeName = this.volume.get("name");
+
+    if (volumeName.hasError("required")) {
+      return "required translated";
+    }
+    if (volumeName.hasError("pattern")) {
+      return "pattern haha where is Shallan translate me";
+    }
+    if (volumeName.hasError("isMounted")) {
+      return "already mounted translate me";
+    }
+  }
+    //Method that disables selecting a mounted pvc
+    private isMountedValidator(): ValidatorFn {
+      return (control: AbstractControl): { [key: string]: any } => {
+        const exists = this.mountedVolumes.has(control.value);
+        return exists ? { isMounted: true } : null;
+      };
+    }
+  }
+  // Error when invalid control is dirty, touched, or submitted
+  export class PvcErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      const isSubmitted = form && form.submitted;
+      //Allows to control when volume is untouched but already assigned
+      return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted || !control.hasError("pattern")));
+    }
+  // END AAW SPECIFIC CHANGES
 }

a379f9139954175461d297337ce1e0fdc374b916
Jose Matsuda <jose.matsuda@statcan.gc.ca>
2022-09-26 14:25:53 -0400

    some stuff

--- frontend/jupyter/src/app/pages/form/form-default/volume/new/new.component.ts
+++ frontend/jupyter/src/app/pages/form/form-default/volume/new/new.component.ts
@@ -42,7 +42,7 @@ export class NewVolumeComponent implements OnInit {
 
   @Input() volGroup: FormGroup;
   @Input() externalName: string;
-  @Input() sizes: Set<string>; // AAW change, this appears to be where you input your chosen size anyways
+  @Input() sizes: Set<string>; // AAW change, has to take place here to control difference between workspace and data vol sizes.
 
   yamlPrv = '';
   errorParsingYaml = '';

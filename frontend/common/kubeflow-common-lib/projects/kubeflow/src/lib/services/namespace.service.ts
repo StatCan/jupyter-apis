2339f43ff3d963d714a48e685af47442d52f7a90
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2022-12-12 20:56:45 +0000

    reverted change

--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/services/namespace.service.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/services/namespace.service.ts
@@ -47,7 +47,7 @@ export class NamespaceService {
       this.dashboardConnectedSource.next(DashboardState.Disconnected);
 
       if (this.currNamespace === undefined) {
-        this.updateSelectedNamespace('mathis-marcotte');
+        this.updateSelectedNamespace('kubeflow-user');
       }
     });
   }

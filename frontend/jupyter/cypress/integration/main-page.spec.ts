f6080eb540c3f6b080a1e3ef42c82732f2c5bd26
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-03-06 13:40:11 +0000

    set up cypress

--- frontend/jupyter/cypress/integration/main-page.spec.ts
+++ frontend/jupyter/cypress/integration/main-page.spec.ts
@@ -1,25 +1,23 @@
-import { settings } from '../fixtures/settings';
-
 describe('Main table', () => {
-  beforeEach(() => {});
+  beforeEach(() => {
+    cy.log("Mocking api calls");
+    cy.mockNamespacesRequest();
+    cy.fixture('settings').then(settings => {
+      cy.mockNotebooksRequest(settings.namespace);
+      cy.mockPVCsRequest(settings.namespace)
+      cy.mockKubecostRequest(settings.namespace)
+    });
+  });
 
-  it('should have a "Notebook Servers" title', () => {
+  it('should have a "Notebooks" title', () => {
     cy.visit('/');
-    cy.get('[data-cy-table-title]')
-      .contains('Notebook Servers')
-      .should('exist');
+    cy.get('[data-cy-toolbar-title]').contains('Notebooks').should('exist');
   });
 
   it('should list Notebooks without errors', () => {
-    cy.selectNamespace(settings.namespace);
-
-    // ensure there's at least one Notebook
-    cy.createNotebook();
-
-    // wait for the request to fetch notebooks
-    const getUrl = `/api/namespaces/${settings.namespace}/notebooks`;
-    cy.intercept('GET', getUrl).as('getNotebooks');
-    cy.wait('@getNotebooks');
+    cy.visit('/');
+    // wait for the request to fetch notebooks and namespaces
+    cy.wait(['@mockNamespacesRequest', '@mockNotebooksRequest']);
 
     // after fetching the data the page should not have an error snackbar
     cy.get('[data-cy-snack-status=ERROR]').should('not.exist');
--- frontend/jupyter/cypress/support/commands.ts
+++ frontend/jupyter/cypress/support/commands.ts
@@ -1,31 +1,25 @@
-import { notebook } from '../fixtures/notebook';
-import { settings } from '../fixtures/settings';
+/// <reference types="cypress" />
 
-Cypress.Commands.add('selectNamespace', ns => {
-  cy.intercept('GET', '/api/namespaces').as('getNamespaces');
-  cy.visit('/');
-
-  cy.log(`Selecting Namespace: ${ns}`);
-  cy.wait('@getNamespaces');
-
-  // click and select the provided namespace
-  cy.get('[data-cy-namespace-selector-dropdown]').click();
-  cy.get(`[data-cy-namespace=${ns}]`).click();
+Cypress.Commands.add('mockNamespacesRequest', () => {
+  cy.intercept('GET', '/api/namespaces', {
+    fixture: 'namespaces',
+  }).as('mockNamespacesRequest');
 });
 
-Cypress.Commands.add('createNotebook', () => {
-  const randomSubfix = Math.random().toString(36).substring(4);
+Cypress.Commands.add('mockNotebooksRequest', namespace => {
+  cy.intercept('GET', `/api/namespaces/${namespace}/notebooks`, {
+    fixture: 'notebooks',
+  }).as('mockNotebooksRequest');
+});
 
-  notebook.name = `test-notebook-${randomSubfix}`;
-  notebook.namespace = settings.namespace;
+Cypress.Commands.add('mockPVCsRequest', namespace => {
+  cy.intercept('GET', `/api/namespaces/${namespace}/pvcs`, {
+    fixture: 'pvcs',
+  }).as('mockPVCsRequest');
+});
 
-  cy.log(`Creating a test Notebook ${notebook.namespace}/${notebook.name}`);
-
-  const method = 'POST';
-  const url = `api/namespaces/${settings.namespace}/notebooks`;
-  const body = notebook;
-
-  cy.request({ method, url, body, headers: settings.postHeaders }).then(() => {
-    return notebook;
-  });
+Cypress.Commands.add('mockKubecostRequest', namespace => {
+  cy.intercept('GET', `/api/namespaces/${namespace}/cost/allocation`, {
+    fixture: 'cost',
+  }).as('mockKubecostRequest');
 });

a98420ba37be40ab161f3c5549293bbef7d73270
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-05-29 21:22:16 +0000

    form-advanced-options

--- frontend/jupyter/src/app/pages/form/form-default/form-advanced-options/form-advanced-options.component.ts
+++ frontend/jupyter/src/app/pages/form/form-default/form-advanced-options/form-advanced-options.component.ts
@@ -1,29 +1,21 @@
-import { Component, Input, AfterViewChecked, OnInit, ChangeDetectorRef, LOCALE_ID, Inject} from "@angular/core";
-import {FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { Component, OnInit, Input, LOCALE_ID, Inject } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-advanced-options',
   templateUrl: './form-advanced-options.component.html',
   styleUrls: ['./form-advanced-options.component.scss'],
 })
-export class FormAdvancedOptionsComponent implements AfterViewChecked, OnInit{
+export class FormAdvancedOptionsComponent implements OnInit {
   @Input() parentForm: FormGroup;
   languageList = [
     {'id':'en', 'label':'English'},
     {'id':'fr', 'label':'Français'}
   ];
-  constructor(@Inject(LOCALE_ID) public localeId: string, private fb: FormBuilder, private cdr: ChangeDetectorRef) {
-    this.parentForm = this.fb.group({
-      language: new FormControl(),
-      shm: new FormControl()
-    });
-  }
 
-  ngOnInit(){
+  constructor(@Inject(LOCALE_ID) public localeId: string) {}
+
+  ngOnInit() {
     this.parentForm.get('language').setValue(this.localeId)
-  }
-
-  ngAfterViewChecked(){
-    this.cdr.detectChanges();
   }
 }

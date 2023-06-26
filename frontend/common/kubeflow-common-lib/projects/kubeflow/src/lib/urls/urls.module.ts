c6182f60bcea41e2ba520319458d7a986e7a629c
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-06-26 18:36:14 +0000

    fixed url link issues

--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/urls/urls.module.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/urls/urls.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
 import { UrlsComponent } from './urls.component';
 
 @NgModule({
   declarations: [UrlsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [UrlsComponent],
 })
 export class UrlsModule {}

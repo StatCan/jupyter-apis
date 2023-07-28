37af01578185f3aab7c8411f83a2d4e753534cae
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-07-28 14:19:45 +0000

    init update of i18n for jwa1.7

--- frontend/jupyter/src/app/services/config.ts
+++ frontend/jupyter/src/app/services/config.ts
@@ -19,7 +19,7 @@ export function getStopDialogConfig(name: string): DialogConfig {
   return {
     title: $localize`Are you sure you want to stop this notebook server? ${name}`,
     message: $localize`Warning: Your data might be lost if the notebook server
-                       is not backed by persistent storage.`,
+                       is not backed by persistent storage`,
     accept: $localize`STOP`,
     confirmColor: 'primary',
     cancel: $localize`CANCEL`,

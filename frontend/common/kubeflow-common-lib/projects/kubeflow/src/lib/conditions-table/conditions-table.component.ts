a02a7d32ba9768de0a81887b988068dd65bb6c72
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2023-08-02 14:25:53 +0000

    added missing translation

--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/conditions-table/conditions-table.component.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/conditions-table/conditions-table.component.ts
@@ -14,6 +14,11 @@ export class ConditionsTableComponent {
   private conditionsPrv: ConditionIR[] = [];
   public config: TableConfig = generateConfig();
 
+  filterHeader=$localize`Filter`;
+  filterPlaceholder=$localize`Enter property name or value`;
+  clearButtonTooltip=$localize`Clear filters`;
+  emptyRows=$localize`No rows to display`;
+
   @Input()
   set title(t: string) {
     this.config.title = t;
--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/resource-table/resource-table.component.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/resource-table/resource-table.component.ts
@@ -23,6 +23,11 @@ export class ResourceTableComponent implements OnInit {
   @Input() data: any[];
   @Input() trackByFn: (index: number, r: any) => string;
 
+  @Input() filterHeader: String = 'Filter';
+  @Input() filterPlaceholder: String = 'Enter property name or value';
+  @Input() clearButtonTooltip: String = 'Clear filters';
+  @Input() emptyRows: String = 'No rows to display';
+
   // Whenever a button in a row is pressed the component will emit an event
   // with information regarding the button that was pressed as well as the
   // row's object.

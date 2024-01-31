931eaa331961304e04edbb00abea69f2db6c3915
Mathis Marcotte <mathis.marcotte@statcan.gc.ca>
2024-01-31 15:52:29 +0000

    fix formatting and lint

--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/form/form.module.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/form/form.module.ts
@@ -8,9 +8,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {
-  MatProgressSpinnerModule,
-} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 

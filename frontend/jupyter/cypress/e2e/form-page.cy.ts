c991063ca0c550750155f959b60f6ba928c69a91
Wendy V Gaultier <wvgaultier@gmail.com>
2024-03-12 12:24:06 +0000

    Fix test

--- frontend/jupyter/cypress/e2e/form-page.cy.ts
+++ frontend/jupyter/cypress/e2e/form-page.cy.ts
@@ -611,12 +611,6 @@ describe('New notebook form', () => {
         .find('[data-cy-form-input="existing-volume"]')
         .find('mat-select')
         .click({ force: true });
-      cy.get('[role="listbox"] > mat-option')
-        .contains('test-pro-b-volume')
-        .click({ force: true });
-      cy.get('[data-cy-form-input="dataVolumes"]')
-        .find('[data-cy-form-input="existing-volume"]')
-        .should('have.class', 'ng-invalid');
       cy.get('[data-cy-form-input="dataVolumes"]')
         .find('[data-cy-form-input="existing-volume"]')
         .should('have.class', 'ng-valid');

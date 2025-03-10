164b2890accfd02fc001073c2e2cafd66fdfbda7
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-03-10 17:46:56 +0000

    fix: fixed various issues that was preventing the app from starting

--- frontend/jupyter/src/app/pages/index/index-default/index-default.module.ts
+++ frontend/jupyter/src/app/pages/index/index-default/index-default.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexDefaultComponent } from './index-default.component';
 import { KubeflowModule } from 'kubeflow';
+import { DefaultComponent } from './default-icon/default-icon.component';
 import { ServerTypeComponent } from './server-type/server-type.component';
 
 @NgModule({
-  declarations: [IndexDefaultComponent, ServerTypeComponent],
-  imports: [CommonModule, KubeflowModule, CommonModule],
-  exports: [IndexDefaultComponent, ServerTypeComponent],
+  declarations: [IndexDefaultComponent, ServerTypeComponent, DefaultComponent],
+  imports: [CommonModule, KubeflowModule],
+  exports: [IndexDefaultComponent, ServerTypeComponent, DefaultComponent],
 })
 export class IndexDefaultModule {}

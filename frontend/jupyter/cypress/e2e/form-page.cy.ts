69d343f0c570cc90f904cf6534a377d0733450c8
Mathis Marcotte <mathis.marcotte@canada.ca>
2025-03-11 17:52:03 +0000

    init update of cypress tests

--- frontend/jupyter/cypress/e2e/form-page.cy.ts
+++ frontend/jupyter/cypress/e2e/form-page.cy.ts
@@ -2,6 +2,7 @@ describe('New notebook form', () => {
   beforeEach(() => {
     cy.mockDashboardRequest();
     cy.mockConfigRequest();
+    cy.mockDefaultStorageClassRequest('standard');
     cy.fixture('settings').then(settings => {
       cy.mockNotebooksRequest(settings.namespace);
       cy.mockPoddefaultsRequest(settings.namespace);
@@ -797,5 +798,54 @@ describe('New notebook form', () => {
       cy.wait('@mockSubmitNotebook');
       cy.url().should('eq', 'http://localhost:4200/');
     });
+  });
+
+  it('should update panel header name according to the name input field', () => {
+    cy.visit('/new');
+
+    cy.get('[data-cy="workspace volume"]').click();
+
+    cy.get('[data-cy="volume name input"]').clear().type('test');
+
+    cy.get('[data-cy="volume name header"]').contains('test');
+  });
+
+  it('should update name input field according to the ConfigMap', () => {
+    cy.visit('/new');
+    cy.wait(['@mockConfigRequest']);
+
+    cy.get('[data-cy="workspace volume"]').click();
+
+    cy.get('[data-cy="volume name input"]').should($nameInput => {
+      const nameValue = $nameInput.val();
+      // '{notebook-name}-workspace' is the name value of the config fixture
+      expect(nameValue).equal('-workspace');
+    });
+  });
+
+  it('should update size input field according to the ConfigMap', () => {
+    cy.visit('/new');
+    cy.wait(['@mockConfigRequest']);
+
+    cy.get('[data-cy="workspace volume"]').click();
+
+    cy.get('[data-cy="size input"]').should($sizeInput => {
+      const sizeValue = $sizeInput.val();
+      // '20Gi' is the storage value of the config fixture
+      expect(sizeValue).equal('20');
+    });
+  });
+
+  it('should update access mode input field according to the ConfigMap', () => {
+    cy.visit('/new');
+    cy.wait(['@mockConfigRequest']);
+
+    cy.get('[data-cy="workspace volume"]').click();
+
+    // 'ReadWriteMany' is the accessModes value of the config fixture
+    cy.get('[data-cy="ReadWriteMany"]').should(
+      'have.class',
+      'mat-radio-checked',
+    );
   });
 });
--- frontend/jupyter/cypress/support/e2e.ts
+++ frontend/jupyter/cypress/support/e2e.ts
@@ -39,13 +39,12 @@ declare global {
       mockNotebooksAllNamespacesRequest(namespace: string): Chainable<void>;
 
       /**
-       * Custom command to mock requests at
-       *  - '/api/storageclasses'
+       * Custom command to mock requests at '/api/storageclasses'
        */
       mockStorageClassesRequests(): Chainable<void>;
 
       /**
-       * Custom command to mock requests at - '/api/storageclasses/default'
+       * Custom command to mock requests at '/api/storageclasses/default'
        * and returns parameter defaultStorageClass
        */
       mockDefaultStorageClassRequest(): Chainable<void>;

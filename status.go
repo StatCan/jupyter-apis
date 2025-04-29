eb02f29293c58416a2dbd1ec9159f164b0fd8863
Mathis Marcotte <84033116+mathis-marcotte@users.noreply.github.com>
2025-04-29 10:12:14 -0400

    fix: Fix various kf1.9 issues from local testing  (#359)
    * fix: removed column formatting for sys lang input
    
    * added key to notebook status
    
    * fixed volume status icon
    
    * fix: added primary color to custom image checkbox
    
    * fix: reverted primary color palette to fix slide toggle styling
    
    * fix: reverted how events are listed to fix the sorting
    
    * updated status keys and messages for conditions and event errors
    
    * updated translations

--- status.go
+++ status.go
@@ -8,17 +8,21 @@ import (
 	kubeflowv1 "github.com/StatCan/kubeflow-apis/apis/kubeflow/v1"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 // notebookPhase is the phase of a notebook.
 type notebookPhase string
+
+// statusKey is a string linking to a status message
+type statusKey string
 
 // status represents the status of a notebook.
 type status struct {
 	Message string        `json:"message"`
 	Phase   notebookPhase `json:"phase"`
 	State   string        `json:"state"`
-	Key     string        `json:"key"`
+	Key     statusKey     `json:"key"`
 }
 
 const (
@@ -49,7 +53,40 @@ const (
 
 const STOP_ANNOTATION = "kubeflow-resource-stopped"
 
-func createStatus(phase notebookPhase, message string, state string, key string) status {
+// defines status message keys, which are used for translations in the frontend
+const (
+	waitingStatus statusKey = "waitingStatus"
+
+	notebookDeleting statusKey = "notebookDeleting"
+
+	notebookStopping statusKey = "notebookStopping"
+
+	noPodsRunning statusKey = "noPodsRunning"
+
+	running statusKey = "running"
+
+	noInformation statusKey = "noInformation"
+
+	errorCondition statusKey = "errorCondition"
+
+	errorEvent statusKey = "errorEvent"
+
+	schedulingPod statusKey = "schedulingPod"
+)
+
+var statusMessages = map[statusKey]string{
+	waitingStatus:    "Waiting for StatefulSet to create the underlying Pod.",
+	noPodsRunning:    "No Pods are currently running for this Notebook Server.",
+	notebookStopping: "Notebook Server is stopping.",
+	notebookDeleting: "Deleting this Notebook Server.",
+	running:          "Running",
+	noInformation:    "Couldn't find any information for the status of this notebook server.",
+	errorCondition:   "An error has occured. Click on the notebook server name for more information.",
+	errorEvent:       "An error has occured. Click on the notebook server name for more information.",
+	schedulingPod:    "Scheduling the Pod.",
+}
+
+func createStatus(phase notebookPhase, message string, state string, key statusKey) status {
 	return status{
 		Phase:   phase,
 		Message: message,
@@ -62,41 +99,41 @@ func createStatus(phase notebookPhase, message string, state string, key string)
 // [ready|waiting|warning|terminating|stopped]
 func (s *server) processStatus(notebook *kubeflowv1.Notebook) (status, error) {
 	// In case the Notebook has no status
-	statusPhase, statusMessage := getEmptyStatus(notebook)
+	statusPhase, statusMessage, statusKey := getEmptyStatus(notebook)
 	if statusPhase != "" {
-		return createStatus(statusPhase, statusMessage, "", ""), nil
+		return createStatus(statusPhase, statusMessage, "", statusKey), nil
 	}
 
 	// In case the Notebook is being stopped
-	statusPhase, statusMessage = getStoppedStatus(notebook)
+	statusPhase, statusMessage, statusKey = getStoppedStatus(notebook)
 	if statusPhase != "" {
-		return createStatus(statusPhase, statusMessage, "", ""), nil
+		return createStatus(statusPhase, statusMessage, "", statusKey), nil
 	}
 
 	// In case the Notebook is being deleted
-	statusPhase, statusMessage = getDeletedStatus(notebook)
+	statusPhase, statusMessage, statusKey = getDeletedStatus(notebook)
 	if statusPhase != "" {
-		return createStatus(statusPhase, statusMessage, "", ""), nil
+		return createStatus(statusPhase, statusMessage, "", statusKey), nil
 	}
 
 	// In case the Notebook is ready
-	statusPhase, statusMessage = checkReadyNotebook(notebook)
+	statusPhase, statusMessage, statusKey = checkReadyNotebook(notebook)
 	if statusPhase != "" {
-		return createStatus(statusPhase, statusMessage, "", ""), nil
+		return createStatus(statusPhase, statusMessage, "", statusKey), nil
 	}
 
 	// Extract information about the status from the containerState of the
 	// Notebook's status
-	statusPhase, statusMessage = getStatusFromContainerState(notebook)
+	statusPhase, statusMessage, statusKey = getStatusFromContainerState(notebook)
 	if statusPhase != "" {
-		return createStatus(statusPhase, statusMessage, "", ""), nil
+		return createStatus(statusPhase, statusMessage, "", statusKey), nil
 	}
 
 	// Extract information about the status from the conditions of the
 	// Notebook's status
-	statusPhase, statusMessage = getStatusFromConditions(notebook)
+	statusPhase, statusMessage, statusKey = getStatusFromConditions(notebook)
 	if statusPhase != "" {
-		return createStatus(statusPhase, statusMessage, "", ""), nil
+		return createStatus(statusPhase, statusMessage, "", statusKey), nil
 	}
 
 	// Try to extract information about why the notebook is not starting
@@ -105,19 +142,19 @@ func (s *server) processStatus(notebook *kubeflowv1.Notebook) (status, error) {
 	if err != nil {
 		return status{}, err
 	}
-	statusEvent, reasonEvent := getStatusFromEvents(notebookEvents)
+	statusEvent, reasonEvent, statusKey := getStatusFromEvents(notebookEvents)
 	if statusEvent != "" {
-		return createStatus(statusEvent, reasonEvent, "", ""), nil
+		return createStatus(statusEvent, reasonEvent, "", statusKey), nil
 	}
 
 	// In case there no Events available, show a generic message
 	statusPhase = NotebookPhaseWarning
-	statusMessage = "Couldn't find any information for the status of this notebook."
+	statusMessage = statusMessages[noInformation]
 
-	return createStatus(statusPhase, statusMessage, "", ""), nil
+	return createStatus(statusPhase, statusMessage, "", noInformation), nil
 }
 
-func getEmptyStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
+func getEmptyStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string, statusKey) {
 	currentTime := time.Now()
 	notebookCreationTime := notebook.CreationTimestamp.Time
 	delta := currentTime.Sub(notebookCreationTime)
@@ -129,67 +166,67 @@ func getEmptyStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
 	// (instead of warning) and we will show a generic message for the first 10 seconds
 	if containerState == (v1.ContainerState{}) && len(conditions) == 0 && delta.Seconds() <= 10 {
 		statusPhase := NotebookPhaseWaiting
-		statusMessage := "Waiting for StatefulSet to create the underlying Pod."
-		return statusPhase, statusMessage
+		statusMessage := statusMessages[waitingStatus]
+		return statusPhase, statusMessage, waitingStatus
 	}
 
-	return "", ""
+	return "", "", ""
 }
 
-func getStoppedStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
+func getStoppedStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string, statusKey) {
 	if _, ok := notebook.Annotations[STOP_ANNOTATION]; ok {
 		// If the Notebook is stopped, the status will be stopped
 		if notebook.Status.ReadyReplicas == 0 {
 			statusPhase := NotebookPhaseStopped
-			statusMessage := "No Pods are currently running for this Notebook Server."
-			return statusPhase, statusMessage
+			statusMessage := statusMessages[noPodsRunning]
+			return statusPhase, statusMessage, noPodsRunning
 		} else {
 			//If the Notebook is being stopped, the status will be waiting
 			statusPhase := NotebookPhaseWaiting
-			statusMessage := "Notebook Server is stopping."
-			return statusPhase, statusMessage
+			statusMessage := statusMessages[notebookStopping]
+			return statusPhase, statusMessage, notebookStopping
 		}
 	}
 
-	return "", ""
+	return "", "", ""
 }
 
-func getDeletedStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
+func getDeletedStatus(notebook *kubeflowv1.Notebook) (notebookPhase, string, statusKey) {
 	// If the Notebook is being deleted, the status will be terminating
 	if notebook.DeletionTimestamp != nil {
 		statusPhase := NotebookPhaseTerminating
-		statusMessage := "Deleting this Notebook Server."
-		return statusPhase, statusMessage
+		statusMessage := statusMessages[notebookDeleting]
+		return statusPhase, statusMessage, notebookDeleting
 	}
 
-	return "", ""
+	return "", "", ""
 }
 
-func checkReadyNotebook(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
+func checkReadyNotebook(notebook *kubeflowv1.Notebook) (notebookPhase, string, statusKey) {
 	// If the Notebook is running, the status will be ready
 	if notebook.Status.ReadyReplicas == 1 {
 		statusPhase := NotebookPhaseReady
-		statusMessage := "Running"
-		return statusPhase, statusMessage
+		statusMessage := statusMessages[running]
+		return statusPhase, statusMessage, running
 	}
 
-	return "", ""
+	return "", "", ""
 }
 
-func getStatusFromContainerState(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
+func getStatusFromContainerState(notebook *kubeflowv1.Notebook) (notebookPhase, string, statusKey) {
 	// https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#container-states
 	// Check the status
 	containerState := notebook.Status.ContainerState
 
 	if containerState.Waiting == nil {
-		return "", ""
+		return "", "", ""
 	}
 
 	// If the Notebook is initializing, the status will be waiting
 	if containerState.Waiting.Reason == "PodInitializing" {
 		statusPhase := NotebookPhaseWaiting
-		statusMessage := containerState.Waiting.Reason
-		return statusPhase, statusMessage
+		statusMessage := statusMessages[schedulingPod]
+		return statusPhase, statusMessage, schedulingPod
 	} else {
 		// In any other case, the status will be warning with a "reason:
 		// message" showing on hover
@@ -206,51 +243,49 @@ func getStatusFromContainerState(notebook *kubeflowv1.Notebook) (notebookPhase, 
 
 		statusMessage := fmt.Sprintf("%s : %s", reason, message)
 
-		return statusPhase, statusMessage
+		return statusPhase, statusMessage, ""
 	}
 }
 
-func getStatusFromConditions(notebook *kubeflowv1.Notebook) (notebookPhase, string) {
+func getStatusFromConditions(notebook *kubeflowv1.Notebook) (notebookPhase, string, statusKey) {
 	for _, condition := range notebook.Status.Conditions {
-		// The status will be warning with a "reason: message" showing on hover
 		if condition.Reason != "" {
 			statusPhase := NotebookPhaseWarning
-			statusMessage := condition.Reason + ": " + condition.Message
-			return statusPhase, statusMessage
+			statusMessage := statusMessages[errorCondition]
+			return statusPhase, statusMessage, errorCondition
 		}
 	}
 
-	return "", ""
+	return "", "", ""
 }
 
 func (s *server) getNotebookEvents(notebook *kubeflowv1.Notebook) ([]*corev1.Event, error) {
-	notebookEvents, err := s.listEvents(notebook.Namespace, "Notebook", notebook.Name)
+	// Load events
+	allevents, err := s.listers.events.Events(notebook.Namespace).List(labels.Everything())
 	if err != nil {
 		return []*corev1.Event{}, err
 	}
 
-	// User can delete and then create a nb server with the same name
-	// Make sure previous events are not taken into account
-	output := make([]*corev1.Event, 0)
-	for _, e := range notebookEvents {
-		if e.CreationTimestamp.Before(&notebook.CreationTimestamp) {
+	// Filter past events
+	events := make([]*corev1.Event, 0)
+	for _, event := range allevents {
+		if event.InvolvedObject.Kind != "Notebook" || event.InvolvedObject.Name != notebook.Name || event.CreationTimestamp.Before(&notebook.CreationTimestamp) {
 			continue
 		}
 
-		output = append(output, e)
+		events = append(events, event)
 	}
+	sort.Sort(eventsByTimestamp(events))
 
-	sort.Sort(eventsByTimestamp(output))
-
-	return output, nil
+	return events, nil
 }
 
-func getStatusFromEvents(notebookEvents []*corev1.Event) (notebookPhase, string) {
+func getStatusFromEvents(notebookEvents []*corev1.Event) (notebookPhase, string, statusKey) {
 	for _, e := range notebookEvents {
 		if e.Type == corev1.EventTypeWarning {
-			return NotebookPhaseWarning, e.Message
+			return NotebookPhaseWarning, statusMessages[errorEvent], errorEvent
 		}
 	}
 
-	return "", ""
+	return "", "", ""
 }

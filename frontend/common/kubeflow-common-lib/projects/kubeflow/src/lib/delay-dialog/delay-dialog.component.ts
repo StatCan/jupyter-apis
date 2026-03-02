4a931d11f48d6aab9b19716b342336453baaa9fc
Wendy Gaultier <wendy.gaultier@statcan.gc.ca>
2026-03-02 14:22:26 +0000

    lint common

--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/delay-dialog/delay-dialog.component.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/delay-dialog/delay-dialog.component.ts
@@ -1,48 +1,61 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DelayDialogConfig, DELAY_DIALOG_RESP } from './types';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'delay-dialog',
   templateUrl: 'delay-dialog.component.html',
 })
-export class DelayDialogComponent implements OnInit{
+export class DelayDialogComponent implements OnInit {
   public DELAY_DIALOG_RESP = DELAY_DIALOG_RESP;
   formDelayCtrl: FormGroup;
   @Input() hourControl: FormControl<number | string>;
 
-  constructor(public delaydialogRef: MatDialogRef<DelayDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DelayDialogConfig) { }
+  constructor(
+    public delaydialogRef: MatDialogRef<DelayDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DelayDialogConfig,
+  ) {}
 
   ngOnInit() {
     const fb = new FormBuilder();
 
     this.formDelayCtrl = fb.group({
-      hourControl: [ '1', [Validators.min(0), Validators.max(72), Validators.pattern('^([1-9]|[1-6]\d|7[0-2])$')]]
+      hourControl: [
+        '1',
+        [
+          Validators.min(0),
+          Validators.max(72),
+          Validators.pattern('^([1-9]|[1-6]d|7[0-2])$'),
+        ],
+      ],
     });
-
   }
 
   onCancelClicked(): void {
-    this.delaydialogRef.close({ status: DELAY_DIALOG_RESP.CANCEL});
+    this.delaydialogRef.close({ status: DELAY_DIALOG_RESP.CANCEL });
   }
   onOkClicked(): void {
-    const h = this.formDelayCtrl.get("hourControl").value.toString();
-    this.delaydialogRef.close({ status: DELAY_DIALOG_RESP.ACCEPT, hours: h});
+    const h = this.formDelayCtrl.get('hourControl').value.toString();
+    this.delaydialogRef.close({ status: DELAY_DIALOG_RESP.ACCEPT, hours: h });
   }
   onNoClick(): void {
     this.onCancelClicked();
   }
 
-   getDelayError(key: string) {
+  getDelayError(key: string) {
     let e: any;
     const errs = this.formDelayCtrl.get(key).errors || {};
 
     if (errs.required) {
       return $localize`Specify an amount of hours for the delay`;
     }
-    if((e =  errs.pattern)) {
+    if ((e = errs.pattern)) {
       return $localize`Specify a full amount of hours for the delay`;
     }
 
@@ -53,6 +66,5 @@ export class DelayDialogComponent implements OnInit{
     if ((e = errs.max)) {
       return $localize`Can't exceed ${e.max} hours`;
     }
-
   }
 }
--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/delay-dialog/delay-dialog.module.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/delay-dialog/delay-dialog.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';

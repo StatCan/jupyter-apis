6abdfe1fba1b60e63f67ee5c0500cf87e70be13f
Mathis Marcotte <mathis.marcotte@canada.ca>
2026-03-05 15:04:46 +0000

    tiny english improvement in code comment

--- frontend/jupyter/src/app/pages/form/form-edit/form-edit.component.ts
+++ frontend/jupyter/src/app/pages/form/form-edit/form-edit.component.ts
@@ -212,7 +212,7 @@ export class FormEditComponent
     };
   }
 
-  // returns only the parent fields from the diff results to be displayed a simple list the fields with differences
+  // returns only the parent fields from the diff results to be displayed as a simple list of the fields with differences
   formatDiff(delta: rdiffResult[]): Set<String> {
     let simpleDiff: Set<String> = new Set();
 

7989db78e91956a2cb696596f936bdd8a7f7b5fd
Wendy Gaultier <wendy.gaultier@statcan.gc.ca>
2026-03-06 12:49:49 +0000

    Fix i18n

--- frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/delay-dialog/delay-dialog.module.ts
+++ frontend/common/kubeflow-common-lib/projects/kubeflow/src/lib/delay-dialog/delay-dialog.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogClose, MatDialogModule } from '@angular/material/dialog';
 import { DelayDialogComponent } from './delay-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [DelayDialogComponent],
@@ -18,6 +19,7 @@ import { DelayDialogComponent } from './delay-dialog.component';
     MatInputModule,
     MatDialogClose,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
 })
 export class DelayDialogModule {}

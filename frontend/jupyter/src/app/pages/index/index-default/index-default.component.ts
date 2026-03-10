4063a4c3bd35fb5a80397d76e309342b36d1f9c5
Wendy Gaultier <wendy.gaultier@statcan.gc.ca>
2026-03-10 11:20:23 +0000

    Update languages

--- frontend/jupyter/src/app/pages/index/index-default/index-default.component.ts
+++ frontend/jupyter/src/app/pages/index/index-default/index-default.component.ts
@@ -286,7 +286,7 @@ export class IndexDefaultComponent implements OnInit, OnDestroy {
       if (!(res === undefined || res.status === DELAY_DIALOG_RESP.CANCEL)) {
         if (res.status === DELAY_DIALOG_RESP.ACCEPT) {
           config.data.msg =
-            $localize`Updating last activity to add ` + res.hours + ` hours`;
+            $localize`Updating last activity to add ${res.hours} hours`;
           this.actions
             .updateKeepAlive(notebook.namespace, notebook.name, res.hours)
             .subscribe(_ => {

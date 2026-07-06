a73cef7b5e49233a74aec7d22e095d4825c80961
Mathis Marcotte <mathis.marcotte@canada.ca>
2026-07-06 13:32:02 +0000

    reverted to default namespace setting

--- frontend/jupyter/src/environments/environment.ts
+++ frontend/jupyter/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000',
-  namespace: 'mathis-marcotte', // change this to your own namespace
+  namespace: 'kubeflow-user', // change this to your own namespace
   resource: 'notebooks',
   ui: 'default',
   jupyterlabLogo: 'assets/logos/jupyterlab-logo.svg',

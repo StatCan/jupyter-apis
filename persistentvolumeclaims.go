eb9e21514e7c4d4a67406f8a94efcf00056df448
Mathis Marcotte <mathis.marcotte@canada.ca>
2026-07-10 19:21:37 +0000

    Added new API to expand the size of a PVC

--- persistentvolumeclaims.go
+++ persistentvolumeclaims.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -119,6 +120,8 @@ const PVCUsageAnnotation string = "pvc.kubeflow.org/usage"
 
 // PVCUsedBytesAnnotation is the annotation name representing the the amount of bytes used in the PVC
 const PVCUsedBytesAnnotation string = "pvc.kubeflow.org/used-bytes"
+
+var validPVCSizes = []string{"4Gi", "8Gi", "16Gi", "32Gi", "64Gi", "128Gi", "256Gi", "512Gi"}
 
 // Set the status of the pvc
 // https://github.com/kubeflow/kubeflow/blob/v1.7.0/components/crud-web-apps/volumes/backend/apps/common/status.py#L4
@@ -540,6 +543,59 @@ func (s *server) GetPvc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s.respond(w, r, resp)
+}
+
+func (s *server) ExpandPvc(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	namespace := vars["namespace"]
+	pvc := vars["pvc"]
+
+	// Read the incoming request data for the new pvc size
+	var expandPvcRequest struct {
+		Size resource.Quantity `json:"size"`
+	}
+	err := s.readRequestBody(w, r, &expandPvcRequest)
+	if err != nil {
+		s.error(w, r, err)
+		return
+	}
+
+	log.Printf("Expanding PVC %s/%s", namespace, pvc)
+
+	// Get the PVC resource
+	vol, err := s.listers.persistentVolumeClaims.PersistentVolumeClaims(namespace).Get(pvc)
+	if err != nil {
+		s.error(w, r, err)
+		return
+	}
+
+	// Validate that the new size is an expected value.
+	// K8s will automatically validate that the new size is
+	// not smaller than the old size of the PVC.
+	newSizeString := expandPvcRequest.Size.String()
+	if !slices.Contains(validPVCSizes, newSizeString) {
+		s.error(w, r, errors.New("Invalid size for PVC"))
+		return
+	}
+
+	// Expand the volume
+	vol.Spec.Resources.Requests[corev1.ResourceStorage] = expandPvcRequest.Size
+	_, err = s.clientsets.kubernetes.CoreV1().PersistentVolumeClaims(namespace).Update(context.TODO(), vol, v1.UpdateOptions{})
+	if err != nil {
+		s.error(w, r, err)
+		return
+	}
+
+	// resp := &getpvcresponse{
+	// 	APIResponseBase: APIResponseBase{
+	// 		Success: true,
+	// 		Status:  http.StatusOK,
+	// 	},
+	// 	Pvc:       *vol,
+	// 	Notebooks: notebooksList,
+	// }
+
+	// s.respond(w, r, resp)
 }
 
 func getPodPvcs(pod corev1.Pod) []string {

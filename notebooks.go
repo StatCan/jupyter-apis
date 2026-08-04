fa0a674291c8542da8ee98b0e590e5c395ebfbc5
Wendy Gaultier <wendy.gaultier@statcan.gc.ca>
2026-08-04 14:40:33 +0000

    Remove log

--- notebooks.go
+++ notebooks.go
@@ -350,7 +350,6 @@ func (s *server) getNotebookData(notebook *kubeflowv1.Notebook) (notebookrespons
 }
 
 func isNotebookPodOOMKilled(nb *kubeflowv1.Notebook, s *server)(bool) {
-	log.Println("getting pod from notebook %q ", nb)
 	notebookNameRequirement, err := labels.NewRequirement("notebook-name", selection.Equals, []string{nb.Name})
 	labelSelector := labels.NewSelector().Add(*notebookNameRequirement)
 	pods, err := s.listers.pods.Pods(nb.Namespace).List(labelSelector)
@@ -377,7 +376,6 @@ func isNotebookPodOOMKilled(nb *kubeflowv1.Notebook, s *server)(bool) {
 
 		if (lastState.Terminated != nil){
 			if (lastState.Terminated.Reason == "OOMKilled"){
-				log.Println("wooof : %" , pod)
 				return true;
 				
 			}

62698913f6fc40a0887e6564e3d094b4f1dbb43d
Mathis Marcotte <mathis.marcotte@canada.ca>
2026-08-12 19:40:43 +0000

    fixed unit test

--- frontend/jupyter/src/app/pages/index/index-default/stopped-time/stopped-time.component.spec.ts
+++ frontend/jupyter/src/app/pages/index/index-default/stopped-time/stopped-time.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { ServerTypeComponent } from './server-type.component';
+import { StoppedTimeComponent } from './stopped-time.component';
 
 describe('ServerTypeComponent', () => {
-  let component: ServerTypeComponent;
-  let fixture: ComponentFixture<ServerTypeComponent>;
+  let component: StoppedTimeComponent;
+  let fixture: ComponentFixture<StoppedTimeComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ServerTypeComponent],
+      declarations: [StoppedTimeComponent],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ServerTypeComponent);
+    fixture = TestBed.createComponent(StoppedTimeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

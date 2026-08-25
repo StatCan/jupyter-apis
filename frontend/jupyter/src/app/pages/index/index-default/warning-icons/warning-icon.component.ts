cf0c7d1a833dcd1c1200a11efd52f916c7b6e41c
Mathis Marcotte <mathis.marcotte@canada.ca>
2026-08-25 12:33:54 +0000

    updated warning icon for volumes

--- frontend/jupyter/src/app/pages/index/index-default/warning-icons/warning-icon.component.ts
+++ frontend/jupyter/src/app/pages/index/index-default/warning-icons/warning-icon.component.ts
@@ -8,25 +8,24 @@ import { TableColumnComponent } from 'kubeflow/lib/resource-table/component-valu
 })
 export class WarningComponent implements TableColumnComponent {
   row: any;
-  textToolTip = '';
 
   constructor() {}
 
   set element(elem: any) {
     this.row = elem;
-    if (this.showWarnings()) {
-      this.textToolTip = this.getWarningsText();
-    }
   }
 
   public showWarnings(): boolean {
-    return this.row?.isOOMKilled ? true : false;
+    return this.getWarningsText() !== '' ? true : false;
   }
 
   public getWarningsText() {
     var thetext = '';
     if (this.row?.isOOMKilled) {
       thetext += '* ' + $localize`Error Out Of Memory Killed.` + '\n';
+    }
+    if (this.row?.fullVolumes && this.row.fullVolumes.length > 0) {
+      thetext += '* ' + $localize`One or more volumes is full.` + '\n';
     }
     return thetext;
   }
